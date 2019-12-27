4771bfff2d439480597c72af52ebb15ed13861e0
郝力衝 <wb-hlc336023@antfin.com>
2019-12-27 17:49:46 +0800

    dev

--- src/pages/CssFilter/index.jsx
+++ src/pages/CssFilter/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './index.less';
 const CssFilter = () => {
-  return <div className={styles.wrap}>
-    filter
-  </div>;
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.mainWrap}>
+        <div className={styles.one}></div>
+        <div className={styles.two}></div>
+      </div>
+    </div>
+  );
 };
 export default CssFilter;
