3b3223bb980d74a666447407a120ab6aeab00fef
郝力衝 <wb-hlc336023@antfin.com>
2020-01-20 15:46:53 +0800

    dev

--- src/pages/Imgcover/index.jsx
+++ src/pages/Imgcover/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'antd';
 import styles from './index.less';
 
 const Imgcover = () => {
+  const [type, setType] = useState('contain');
   return (
     <div
       style={{
         height: '100%',
       }}
     >
-      <div className={styles.imgWrap}></div>
-      'background-size: contain'
+      <div
+        className={styles.imgWrap}
+        style={{
+          backgroundSize: type,
+        }}
+      ></div>
+      <div>
+        'background-size: {type}'
+        <Button
+          onClick={() => {
+            setType(type === 'contain' ? 'cover' : 'contain');
+          }}
+        >
+          切换为{type === 'contain' ? 'cover' : 'contain'}
+        </Button>
+      </div>
     </div>
   );
 };
