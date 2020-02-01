30d94cd2b2bf3b4f29024fbfbe70a01750cd6ed8
faruxue <faruxue2019@163.com>
2020-02-01 14:08:25 +0800

    dev

--- src/service/coronavirus.js
+++ src/service/coronavirus.js
@@ -1,19 +1,39 @@
 import request from './../utils/request';
 
 export const fetchVirusData = async () => {
-  const res = await request('https://www.easy-mock.com/mock/5e3103a2b6f7d03b38a940df/example/api/coronavirus');
-  // const res = await request('/api/coronavirus');
+  // const res = await request('https://www.easy-mock.com/mock/5e3103a2b6f7d03b38a940df/example/api/coronavirus');
+  const res = await request('/api/coronavirus');
   const { data } = res;
-  const newData = data.map((el, idx) => {
-    let value = null;
-    if (data[idx - 1] && data[idx - 1].value) {
-      value = el.value - data[idx - 1].value
-    }
-    return {
+  const newData = [];
+  data.forEach((el, idx) => {
+    newData.push({
       date: el.date,
-      value,
-      type: '较昨日增长值'
+      value: el.value,
+      type: '感染人数'
+    }); // 感染
+    if (data[idx - 1] && data[idx - 1].value) {
+      newData.push({
+        date: el.date,
+        value: el.value - data[idx - 1].value,
+        type: '较昨日新增感染'
+      }); // 较昨日感染
+    } else {
+      newData.push({
+        date: el.date,
+        value: null,
+        type: '较昨日新增感染'
+      }); // 较昨日感染
     }
+    newData.push({
+      date: el.date,
+      value: el.died,
+      type: '死亡人数'
+    }); // 死亡
+    newData.push({
+      date: el.date,
+      value: el.cure,
+      type: '治愈人数'
+    }); // 治愈
   })
-  return [...data, ...newData];
+  return newData;
 }
